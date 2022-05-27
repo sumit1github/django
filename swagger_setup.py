71c94888373d2dd83f09950021b73ea7024129c6
sumit1github <102658947+sumit1github@users.noreply.github.com>
2022-05-27 15:01:04 +0530

    swagger api intregation

--- swagger_setup.py
+++ swagger_setup.py
@@ -1,74 +1,31 @@
-from drf_yasg.utils import swagger_auto_schema
+1. pip install -U drf-yasg
+
+2. settings.py
+
+    INSTALLED_APPS = [
+       ...
+       'django.contrib.staticfiles',  # required for serving swagger ui's css/js files
+       'drf_yasg',
+       ...
+    ]
+
+ 3. project.urls
+
 from rest_framework import permissions
-from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from re_path
-Install drf_yasg
-
-pip install drf_yasg
-
-Add this in settings.py installed apps after django.config.staticfiles
-
-'drf_yasg',
-
-Add schema and urls in urls
-
-Import this lines
-
-
-Add this above url patterns
+from drf_yasg import openapi
 
 schema_view = get_schema_view(
-    openapi.Info(title="ESG APIs", default_version="v1",
-                 description="ESG APIs"),
+    openapi.Info(
+        title="Give your title",
+        default_version='v1',
+        description="service",
+    ),
     public=True,
     permission_classes=[permissions.AllowAny],
 )
 
-
-In url patterns add this
-
-re_path(
-    r"^swagger(?P<format>\.json|\.yaml)$",
-    schema_view.without_ui(cache_timeout=0),
-    name="schema-json",
-),
-re_path(
-    r"^swagger/$",
-    schema_view.with_ui("swagger", cache_timeout=0),
-    name="schema-swagger-ui",
-),
-re_path(
-    r"^redoc/$",
-    schema_view.with_ui("redoc", cache_timeout=0),
-    name="schema-redoc",
-),
-
-
-For Custom parameters and body add in views
-
-
-GET METHOD - PARAMS
-
-lesson_id = openapi.Parameter(
-    "lesson_id", openapi.IN_QUERY, description="Lesson Id", type="id"
-)
-
-
-@swagger_auto_schema(
-    method="get",
-    manual_parameters=[lesson_id],
-)
-POST METHOD - BODY
-
-
-@swagger_auto_schema(
-    method="post",
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            "status": openapi.Schema(type="string", description="status"),
-            "is_video": openapi.Schema(type="string", description="true or false"),
-        },
-    ),
-)
+urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
+ 4.
