3a56bf7798a0c3fbcd0f165291db1021f808a01f
sumit1github <102658947+sumit1github@users.noreply.github.com>
2022-09-06 09:18:02 +0530

    Update swagger_setup.py

--- swagger_setup.py
+++ swagger_setup.py
@@ -42,4 +42,50 @@ urlpatterns = [
         name="schema-redoc",
     ),
     
- 4.
+ ******************************************************** customization*****************************************
+############################ create swagger_data.py in every applications #################################
+from drf_yasg import openapi
+  
+    
+# simple post request
+post_org_creation_page_params = [
+
+    openapi.Parameter(
+        "name", openapi.IN_QUERY, required=True, type=openapi.TYPE_STRING
+    ),
+    openapi.Parameter(
+        "address", openapi.IN_QUERY, required=True, type=openapi.TYPE_STRING
+    ),
+    openapi.Parameter(
+        "Number of Users", openapi.IN_QUERY, required=True, type=openapi.TYPE_STRING
+    ),
+    openapi.Parameter(
+        "country", openapi.IN_QUERY, required=True, type=openapi.TYPE_STRING
+    ),
+]
+
+# something need to pass in header
+ organization_params_in_header = openapi.Parameter(
+    "org", openapi.IN_HEADER, required=True, type=openapi.TYPE_INTEGER
+)
+    
+# get request with header
+account_get_params = [
+    organization_params_in_header, # we can remove it if nothing to pass into header
+    openapi.Parameter("name", openapi.IN_QUERY, type=openapi.TYPE_STRING),
+    openapi.Parameter("city", openapi.IN_QUERY, type=openapi.TYPE_STRING),
+    openapi.Parameter("tags", openapi.IN_QUERY, type=openapi.TYPE_STRING),
+]
+
+############################ views.py ############################
+from drf_yasg.utils import swagger_auto_schema
+from helper import swagger_params
+class ViewName(APIView):
+    permission_classes = (IsAuthenticated,)
+    
+    # both for get and post, same code code need to write 2 times, 1 before get and 2 before post function
+    @swagger_auto_schema(
+        tags=["auth"],
+        operation_description="Your api description",
+        manual_parameters=swagger_params.post_org_creation_page_params,
+    )
