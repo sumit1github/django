bd39b092c10f608579bee35bf0c021ed1d4dd56c
Sumit <102658947+sumit1github@users.noreply.github.com>
2023-04-09 00:09:35 +0530

    Update filesystem.py

--- filesystem.py
+++ filesystem.py
@@ -27,14 +27,27 @@ myfile = request.FILES['file']
 
 # Generate a unique filename for the file
 filename = myfile.name
-s3 = boto3.resource('s3',
-                    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-                    region_name=settings.AWS_S3_REGION_NAME,
-                    endpoint_url=settings.AWS_S3_ENDPOINT_URL)
-s3_bucket = s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
-s3_object = s3_bucket.Object(filename)
-s3_object.upload_fileobj(myfile)
 
-# Return a JSON response with the S3 URL of the uploaded file
-file_url = f"{settings.AWS_S3_CUSTOM_DOMAIN}/{filename}"
+s3_file_upload(f"media/topalert/{month_num}-{year}", filename, image)
+
+def s3_file_upload(folder_name ,filename, file_obj):
+    import uuid
+    import boto3
+    from django.conf import settings
+    
+    filename = str(int(str(uuid.uuid4().int)[:5])) + filename
+    s3 = boto3.resource('s3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME,
+                        
+    )
+    
+    s3_bucket = s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
+    s3_object = s3_bucket.Object(f"{folder_name}/{filename}")
+    s3_object.upload_fileobj(file_obj)
+    
+    # Return the S3 URL of the uploaded file
+    file_path = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{folder_name}/{filename}"
+    
+    return file_path
