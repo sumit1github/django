f3f9ca4c43d4be62eee82464631fa65b5e8ed528
Sumit <102658947+sumit1github@users.noreply.github.com>
2023-05-13 13:32:53 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,3 +1,5 @@
+        from django.utils.http import urlencode
+        
         return redirect(
             reverse("app1_product_master:cart_list")+'?'+urlencode({"quote_id":quote_id, "node":node})
         )
