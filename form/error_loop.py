ac722e08394e16fa54f65f3fcb8b9d2651f34c85
Sumit <102658947+sumit1github@users.noreply.github.com>
2023-06-16 12:32:42 +0530

    Update error_loop.py

--- form/error_loop.py
+++ form/error_loop.py
@@ -1,9 +1,3 @@
-error_list = []
-for field, errors in form.errors.items():
-    for error in errors:
-        error_list.append(f'{field}: {error}')
-
-       
 for field, errors in form.errors.items():
     for error in errors:
         messages.error(request, f'{field}: {error}')
