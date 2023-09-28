2eabba9053cf2fb2166caa13c18a7067d40a8f3b
Sumit <102658947+sumit1github@users.noreply.github.com>
2023-09-28 10:37:36 +0530

    Update custome_user.py

--- custome_user.py
+++ custome_user.py
@@ -46,7 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(null=True,blank=True, unique=True)
 	first_name = models.CharField(max_length=100, null=True)
 	last_name = models.CharField(max_length=100, null=True)
-	
+	is_superuser = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
 	created_on = models.DateField(auto_now_add=True)
 	is_active = models.BooleanField(default=True)
